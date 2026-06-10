57d23124d9c1ba5b16fdd912e56cf2405414a277
Khumbo Lihonga <klihonga@d-tree.org>
2026-06-10 08:46:51 +0200

    Sign in using form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,19 +5,68 @@ import {
   PasswordInput,
   Button,
 } from '@mantine/core';
+import { useForm } from '@mantine/form';
 import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
+  type LoginFormValues = {
+    username: string;
+    password: string;
+  };
+
   const navigate = useNavigate();
+  const form = useForm({
+    mode: 'controlled',
+    initialValues: { username: '', password: '' },
+  });
+
+  // logs the user into the system
+  const handleSubmit = async (values: LoginFormValues) => {
+    try {
+      const { username, password } = values;
+      console.log(`${username}, ${password}`);
+
+      const response = await fetch('/api/me', {
+        headers: {
+          Authorization: `Basic ${btoa(`${username}:${password}`)}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`${response.status} - ${response.statusText}`);
+      }
+
+      const user = await response.json();
+      console.log(user);
+      navigate('/home');
+    } catch (error) {
+      console.log('Failed to login.', error);
+    }
+  };
 
   return (
     <Container size={420} my={40} fluid>
-      <Paper shadow="sm" p={22} mt={30} radius="md">
-        <TextInput label="Username" required radius="md" />
-        <PasswordInput label="Password" required mt="md" radius="md" />
-        <Button fullWidth mt="xl" radius="md" onClick={() => navigate('/home')}>
-          Sign in
-        </Button>
+      <Paper shadow="md" p={22} mt={30} radius="md">
+        <form onSubmit={form.onSubmit(handleSubmit)}>
+          <TextInput
+            key={form.key('username')}
+            {...form.getInputProps('username')}
+            label="Username"
+            required
+            radius="md"
+          />
+          <PasswordInput
+            key={form.key('password')}
+            {...form.getInputProps('password')}
+            label="Password"
+            required
+            mt="md"
+            radius="md"
+          />
+          <Button fullWidth mt="xl" radius="md" type="submit">
+            Sign in
+          </Button>
+        </form>
       </Paper>
     </Container>
   );
