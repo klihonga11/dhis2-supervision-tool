892cbb523770a5e2faf53db3ca369dc77028a6f8
Khumbo Lihonga <klihonga@d-tree.org>
2026-06-22 14:39:01 +0200

    Update types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -9,6 +9,24 @@ export type UserGroupsResponse = {
   ];
 };
 
+export type UserResponse = {
+  id: string;
+  organisationUnits: [{ id: string }];
+};
+
+export type EventResponse = {
+  instances: [
+    {
+      event: string;
+      status: string;
+      createdAt: string;
+      createdBy: {
+        uid: string;
+      };
+    },
+  ];
+};
+
 export type SignedInUser = {
   id: string;
   firstName: string;
@@ -18,4 +36,16 @@ export type SignedInUser = {
 export type AssignedUser = {
   id: string;
   name: string;
+  lastSyncDate?: string;
+};
+
+export type SupervisorGroup = {
+  displayName: string;
+  id: string;
+  users: [
+    {
+      id: string;
+      name: string;
+    },
+  ];
 };
